74e968303bd8feb586737af178457fe7a0d95495
lxshwyan <327251174@qq.com>
2019-05-23 10:09:34 +0800

    远程读取日志

---
+++ LambdaTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace LambdaTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Expression<Func<int, bool>> func = i => i == 11 && true;
+            Console.WriteLine(func.ToString());
+            Console.WriteLine(func.ReturnType.ToString());
+            Console.WriteLine(func.Body);
+            foreach (var item in func.Parameters)
+            {
+                Console.WriteLine($"Name:{item.Name},Type:{item.Type}");
+            }
+
+
+            ConstantExpression ce1 = Expression.Constant(10);
+            Expression<Func<int>> exp1 = Expression.Lambda<Func<int>>(ce1);
+            Console.WriteLine(exp1.ToString());
+             Console.WriteLine(exp1.Compile().Invoke());
+
+
+        }
+    }
+}
--- QuartzDemo/Startup.cs
+++ QuartzDemo/Startup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NLog.Extensions.Logging;
@@ -57,9 +61,30 @@ namespace QuartzDemo
             //读取Nlog配置文件 
             env.ConfigureNLog("nlog.config");
             #endregion
-          EnginContext.Initialize(new GeneralEngine(app.ApplicationServices));
+            EnginContext.Initialize(new GeneralEngine(app.ApplicationServices));
             //获取前面注入的Quartz调度类
             var quartz = app.ApplicationServices.GetRequiredService<QuartzStartup>();
+            #region 远程读取日志
+            FileExtensionContentTypeProvider provider = new FileExtensionContentTypeProvider(); 
+          //  provider.Mappings[".log"] = "text/plain;charset=utf-8";
+          provider.Mappings[".log"] = "text/plain;charset=utf-8";
+            string basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(basePath, "logs")),
+                ServeUnknownFileTypes = true,
+                RequestPath = new PathString("/logs"),
+                ContentTypeProvider = provider,
+                DefaultContentType = "application/x-msdownload", // 设置未识别的MIME类型一个默认z值
+
+            });
+            app.UseDirectoryBrowser(new DirectoryBrowserOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(basePath, "logs")),
+                RequestPath = new PathString("/logs"),
+            });
+            #endregion
+
             appLifetime.ApplicationStarted.Register(() =>
              {
               quartz.Start().Wait(); //网站启动完成执行
